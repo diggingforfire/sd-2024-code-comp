ad3b3271874ccb9381567b07c54548ded7dcfaab
luijben <luijben@astron.nl>
2024-10-23 21:39:30 +0200

    stuff

--- sd_mascot/pet/urls.py
+++ sd_mascot/pet/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import mascot_index, feed_mascot, play_with_mascot, get_mascot_state
+from .views import mascot_index, feed_mascot, play_with_mascot, update_mascot_state
 
 urlpatterns = [
     path('', mascot_index, name='mascot_index'),
     path('feed/', feed_mascot, name='feed_mascot'),
     path('play/', play_with_mascot, name='play_with_mascot'),
-    path('get_state/', get_mascot_state, name='get_mascot_state'),
+    path('update_state/', update_mascot_state, name='update_mascot_state'),
 ]
--- sd_mascot/pet/views.py
+++ sd_mascot/pet/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_POST, require_GET
+
 from .models import Pet
 
 def mascot_index(request):
@@ -16,18 +18,18 @@ def sd_mascot_props():
             'name': sd_mascot().name,
         }
 
+@require_POST
 def feed_mascot(request):
-    if request.method == 'POST':
-        sd_mascot().feed()
-        return JsonResponse(sd_mascot_props())
+    sd_mascot().feed()
+    return JsonResponse(sd_mascot_props())
 
+@require_POST
 def play_with_mascot(request):
-    if request.method == 'POST':
-        sd_mascot().play()
-        return JsonResponse(sd_mascot_props())
+    sd_mascot().play()
+    return JsonResponse(sd_mascot_props())
 
-def get_mascot_state(request):
-    if request.method == 'GET':
-        sd_mascot().increase_hunger()
-        sd_mascot().decrease_happiness()
-        return JsonResponse(sd_mascot_props())
+@require_GET
+def update_mascot_state(request):
+    sd_mascot().increase_hunger()
+    sd_mascot().decrease_happiness()
+    return JsonResponse(sd_mascot_props())
